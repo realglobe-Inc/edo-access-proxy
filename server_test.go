realglobe-Inc/edo-id-provider

データアクセスドライバの改訂
mongodb が一時的に落ちていても再接続するようにした。
キャッシュの期限を新鮮とみなす期限と廃棄する期限に分けた。
KVS にキーを列挙する機能を追加。

server_test.go
@@ -30,9 +30,9 @@ func TestBoot(t *testing.T) {
 		TaKeyProvider:         NewMemoryTaKeyProvider(0),
 		UserNameIndex:         NewMemoryUserNameIndex(0),
 		UserAttributeRegistry: NewMemoryUserAttributeRegistry(0),
-		sessCont:              driver.NewMemoryTimeLimitedKeyValueStore(0),
-		codeCont:              driver.NewMemoryTimeLimitedKeyValueStore(0),
-		accTokenCont:          driver.NewMemoryTimeLimitedKeyValueStore(0),
+		sessCont:              driver.NewMemoryTimeLimitedKeyValueStore(0, 0),
+		codeCont:              driver.NewMemoryTimeLimitedKeyValueStore(0, 0),
+		accTokenCont:          driver.NewMemoryTimeLimitedKeyValueStore(0, 0),
 		maxSessExpiDur:        time.Hour,
 		codeExpiDur:           time.Hour,
 		accTokenExpiDur:       time.Hour,

realglobe-Inc/edo-access-proxy

server_test.go
@@ -88,11 +88,11 @@ func init() {
 
 func newTestSystem() *system {
 	return &system{
-		priKeyCont: driver.NewMemoryKeyValueStore(0),
+		priKeyCont: driver.NewMemoryKeyValueStore(0, 0),
 		taId:       "ta-no-id",
 		hashName:   "sha256",
-		sessCont:   driver.NewMemoryTimeLimitedKeyValueStore(0),
-		cliCont:    driver.NewMemoryTimeLimitedKeyValueStore(0),
+		sessCont:   driver.NewMemoryTimeLimitedKeyValueStore(0, 0),
+		cliCont:    driver.NewMemoryTimeLimitedKeyValueStore(0, 0),
 		threSize:   8192,
 	}
 }