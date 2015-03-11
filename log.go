realglobe-Inc/edo-id-provider

ログライブラリの仕様変更への対応

log.go
@@ -4,8 +4,4 @@ import (
 	"github.com/realglobe-Inc/go-lib-rg/rglog"
 )
 
-var log rglog.Logger
-
-func init() {
-	log = rglog.GetLogger("github.com/realglobe-Inc/edo/edo-id-provider")
-}
+var log = rglog.Logger("github.com/realglobe-Inc/edo/edo-id-provider")

realglobe-Inc/edo-access-proxy

log.go
@@ -4,8 +4,4 @@ import (
 	"github.com/realglobe-Inc/go-lib-rg/rglog"
 )
 
-var log rglog.Logger
-
-func init() {
-	log = rglog.GetLogger("github.com/realglobe-Inc/edo/edo-access-proxy")
-}
+var log = rglog.Logger("github.com/realglobe-Inc/edo/edo-access-proxy")
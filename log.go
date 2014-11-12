579d00e0b8bede1e5336bdbeff71405e9e810fa3
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-11-12 15:45:24 +0900

    ログライブラリの仕様変更への対応

--- log.go
+++ log.go
@@ -4,8 +4,4 @@ import (
 	"github.com/realglobe-Inc/go-lib-rg/rglog"
 )
 
-var log rglog.Logger
-
-func init() {
-	log = rglog.GetLogger("github.com/realglobe-Inc/edo/edo-access-proxy")
-}
+var log = rglog.Logger("github.com/realglobe-Inc/edo/edo-access-proxy")
